00bd4b2305cddcae30c7591d7c3b1072b25d63ef
mromsel <miriamromero2@gmail.com>
2024-03-01 00:00:17 +0100

    Add logic to verify if user is logged to show the login screen or the main app screen

--- lingopals_app/lib/main.dart
+++ lingopals_app/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lingopals_app/l10n/l10n.dart';
+import 'package:lingopals_app/screens/login_screen.dart';
+import 'package:lingopals_app/screens/main_screen.dart';
+import 'package:lingopals_app/services/security_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +22,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: FutureBuilder<bool>(
+        future: SecurityService()
+            .isUserLogged(), // Llama al método para verificar si el usuario está identificado
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Si la conexión está en espera, se muestra un indicador de carga
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (snapshot.hasError) {
+              // Se muestra un mensaje en caso de error
+              return Text('Error: ${snapshot.error}');
+            } else {
+              // Si el usuario está identificado, muestra la pantalla principal
+              if (snapshot.data == true) {
+                return const MainScreen();
+              } else {
+                // Si el usuario no está identificado, muestra la pantalla de inicio de sesión
+                return const LoginScreen();
+              }
+            }
+          }
+        },
+      ),
       supportedLocales: L10n.all,
       locale: const Locale('en'),
       localizationsDelegates: const [
