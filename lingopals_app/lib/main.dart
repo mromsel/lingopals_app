e2653254d69e4cff27fcd041700e2a9a62f8ad60
mromsel <miriamromero2@gmail.com>
2024-03-01 01:08:26 +0100

    Add splash screen to init app logic and error screen

--- lingopals_app/lib/main.dart
+++ lingopals_app/lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lingopals_app/l10n/l10n.dart';
-import 'package:lingopals_app/screens/login_screen.dart';
-import 'package:lingopals_app/screens/main_screen.dart';
-import 'package:lingopals_app/services/security_service.dart';
+import 'package:lingopals_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,31 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: FutureBuilder<bool>(
-        future: SecurityService()
-            .isUserLogged(), // Llama al método para verificar si el usuario está identificado
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // Si la conexión está en espera, se muestra un indicador de carga
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            if (snapshot.hasError) {
-              // Se muestra un mensaje en caso de error
-              return Text('Error: ${snapshot.error}');
-            } else {
-              // Si el usuario está identificado, muestra la pantalla principal
-              if (snapshot.data == true) {
-                return const MainScreen();
-              } else {
-                // Si el usuario no está identificado, muestra la pantalla de inicio de sesión
-                return const LoginScreen();
-              }
-            }
-          }
-        },
-      ),
+      home: const SplashScreen(),
       supportedLocales: L10n.all,
       locale: const Locale('en'),
       localizationsDelegates: const [
@@ -55,42 +29,6 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          AppLocalizations.of(context)!.lingopals_title,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.welcome,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
