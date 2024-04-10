dc0ad223d0d7595f7980c8ed566423b09ff179d4
mromsel <miriamromero2@gmail.com>
2024-04-10 14:34:48 +0200

    Add basic nav bar, pages and authGuard

--- lingopals_app/src/app/app-routing.module.ts
+++ lingopals_app/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/authGuard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'lessons',
+    loadChildren: () => import('./features/lessons/lessons.module').then(m => m.LessonsPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'dictionary',
+    loadChildren: () => import('./features/dictionary/dictionary.module').then(m => m.DictionaryPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./features/settings/settings.module').then(m => m.SettingsPageModule),
+    canActivate: [AuthGuard]
   }
 ];
 @NgModule({
@@ -13,4 +39,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- lingopals_app/src/app/app.module.ts
+++ lingopals_app/src/app/app.module.ts
@@ -6,11 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TabsBarComponent } from './core/tabs-bar/tabs-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    TabsBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
