adf76e323357f77fb2a216017275617c546ae3ba
mromsel <miriamromero2@gmail.com>
2024-04-10 17:31:15 +0200

    Change routes order

--- lingopals_app/src/app/app-routing.module.ts
+++ lingopals_app/src/app/app-routing.module.ts
@@ -4,15 +4,6 @@ import { AuthGuard } from './shared/guards/authGuard';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./features/login/login.module').then(m => m.LoginPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomePageModule),
     canActivate: [AuthGuard]
@@ -31,7 +22,16 @@ const routes: Routes = [
     path: 'settings',
     loadChildren: () => import('./features/settings/settings.module').then(m => m.SettingsPageModule),
     canActivate: [AuthGuard]
-  }
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
 ];
 @NgModule({
   imports: [
