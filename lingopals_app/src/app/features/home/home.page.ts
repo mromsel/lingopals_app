ae96cb7075dfd8a1d0d0e14163dcaaaafa4e9742
mromsel <miriamromero2@gmail.com>
2024-04-14 19:00:47 +0200

    Add basic logic to retrieve user data in home page

--- lingopals_app/src/app/features/home/home.page.ts
+++ lingopals_app/src/app/features/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from 'src/app/shared/interfaces/user-info.interface';
+import { UserProgress } from 'src/app/shared/interfaces/user-progress.interface';
+import { UserInfoService } from 'src/app/shared/services/user-info.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  userInfo: UserInfo | undefined;
+  userProgress: UserProgress | undefined;
+
+  constructor(private userInfoService: UserInfoService) { }
 
   ngOnInit() {
+    this.userInfoService.getUserInfo(1).subscribe(userInfo => {
+      this.userInfo = userInfo;
+      this.userInfoService.getUserProgress(userInfo.idUser).subscribe(userProgress => {
+        this.userProgress = userProgress
+      }
+      )
+    })
   }
 
 }
