582e070653587952b7fc51a2d54f4b8f78b3e535
mromsel <miriamromero2@gmail.com>
2024-04-14 18:56:30 +0200

    Add app and backend url to enviroment.ts

--- lingopals_app/src/environments/environment.ts
+++ lingopals_app/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  'appUrl': 'http://localhost:8100',
+  'backendURL': 'http://127.0.0.1:8080/api'
 };
 
 /*
