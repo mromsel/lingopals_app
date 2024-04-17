b19eed4052ee86ce3ea93f5dca4a97549f9a6ab5
mromsel <miriamromero2@gmail.com>
2024-04-17 22:47:52 +0200

    Receive user id in home page to display data

--- lingopals_app/src/app/features/home/home.page.ts
+++ lingopals_app/src/app/features/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserInfo } from 'src/app/shared/interfaces/user-info.interface';
 import { UserProgress } from 'src/app/shared/interfaces/user-progress.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserInfoService } from 'src/app/shared/services/user-info.service';
 
 @Component({
@@ -10,17 +12,30 @@ import { UserInfoService } from 'src/app/shared/services/user-info.service';
 })
 export class HomePage implements OnInit {
 
+  idUser: number = 0;
   userInfo: UserInfo | undefined;
   userProgress: UserProgress | undefined;
 
-  constructor(private userInfoService: UserInfoService) { }
+  constructor(
+    private authService: AuthService,
+    private userInfoService: UserInfoService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.userInfoService.getUserProgress(1).subscribe(userProgress => {
-      this.userProgress = userProgress
-      this.userInfo = userProgress.user
+    this.idUser = this.authService.getIdUser()
+
+    if (this.idUser != 0) {
+      this.userInfoService.getUserProgress(this.idUser).subscribe(userProgress => {
+        this.userProgress = userProgress
+        this.userInfo = userProgress.user
+      }
+      )
+    } else {
+      this.authService.logout()
+      this.router.navigate(["/login"])
     }
-    )
+
   }
 
   goToShop() {
