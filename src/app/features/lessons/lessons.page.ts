924b8522ec02cff8dff0eccb5b31a57776d8eeb5
mromsel <miriamromero2@gmail.com>
2024-04-24 18:09:43 +0200

    Add method ionViewWillLeave on pages to unsubscribe

--- src/app/features/lessons/lessons.page.ts
+++ src/app/features/lessons/lessons.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Lesson } from 'src/app/shared/interfaces/lesson.interface';
 import { LessonsService } from './services/lessons.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-lessons',
@@ -9,22 +10,30 @@ import { LessonsService } from './services/lessons.service';
 })
 export class LessonsPage implements OnInit {
 
+  unsubscribe$: Subject<void> = new Subject<void>();
+
   //TODO: CHANGE TO LESSONDISPLAY
   lessons: Array<Lesson> = new Array();
 
   constructor(private lessonsService: LessonsService) { }
 
   ngOnInit() {
-    this.lessonsService.getLesson(1).subscribe(lesson => {
+    this.lessonsService.getLesson(1)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(lesson => {
 
-      // AUX
-      for (let index = 0; index < 10; index++) {
-        let auxLesson = structuredClone(lesson)
-        auxLesson.idLesson = index
-        auxLesson.lessonName = "Lesson " + index
-        this.lessons.push(auxLesson)
-      }
-    })
+        // AUX
+        for (let index = 0; index < 10; index++) {
+          let auxLesson = structuredClone(lesson)
+          auxLesson.idLesson = index
+          auxLesson.lessonName = "Lesson " + index
+          this.lessons.push(auxLesson)
+        }
+      })
   }
 
+  ionViewWillLeave() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
--- src/app/features/login/login.page.ts
+++ src/app/features/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { UserLogin } from 'src/app/shared/interfaces/user-login';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -9,6 +10,8 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
+  unsubscribe$: Subject<void> = new Subject<void>();
 
   formData = {
     usernameOrEmail: '',
@@ -29,25 +32,23 @@ export class LoginPage implements OnInit {
   login() {
     let userToSend: UserLogin = new UserLogin(this.formData.usernameOrEmail, this.formData.password, Intl.DateTimeFormat().resolvedOptions().timeZone)
 
-    this.authService.login(userToSend).subscribe({
-      next: (data) => {
-        this.loggingFailed = false
-        this.authService.setIdUser(data.idUser)
+    this.authService.login(userToSend)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (data) => {
+          this.loggingFailed = false
+          this.authService.setIdUser(data.idUser)
 
-        localStorage.setItem("token", data.token);
-        this.authService.setIsUserLogged(true)
+          localStorage.setItem("token", data.token);
+          this.authService.setIsUserLogged(true)
 
-        // this.router.navigate(["/home"])
-        this.router.navigate(["/lessons"])
-        // this.router.navigate(["/dictionary"])
-        // this.router.navigate(["/settings"])
-
-      },
-      error: () => {
-        console.error();
-        this.loggingFailed = true
-      }
-    })
+          this.router.navigate(["/app"])
+        },
+        error: () => {
+          console.error();
+          this.loggingFailed = true
+        }
+      })
   }
 
   goToSignUp() {
@@ -60,4 +61,8 @@ export class LoginPage implements OnInit {
     this.router.navigate(['/forgot-password']);
   }
 
+  ionViewWillLeave() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
