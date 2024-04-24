1238edacfc48fff08bf8b6c57e8622559d62fb72
mromsel <miriamromero2@gmail.com>
2024-04-24 17:59:52 +0200

    Change tabs bar component to page and configure routes

---
+++ src/app/features/tabs/tabs-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+import { AuthGuard } from 'src/app/shared/guards/authGuard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      // home
+      {
+        path: 'home',
+        loadChildren: () => import('../../features/home/home.module').then(m => m.HomePageModule),
+        canActivate: [AuthGuard]
+      },
+      // lessons
+      {
+        path: 'lessons',
+        loadChildren: () => import('../../features/lessons/lessons.module').then(m => m.LessonsPageModule),
+        canActivate: [AuthGuard]
+      },
+      // dictionary
+      {
+        path: 'dictionary',
+        loadChildren: () => import('../../features/dictionary/dictionary.module').then(m => m.DictionaryPageModule),
+        canActivate: [AuthGuard]
+      },
+      // settings
+      {
+        path: 'settings',
+        loadChildren: () => import('../../features/settings/settings.module').then(m => m.SettingsPageModule),
+        canActivate: [AuthGuard]
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ]
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule { }
