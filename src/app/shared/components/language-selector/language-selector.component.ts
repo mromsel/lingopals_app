75ecde65c2bcc04dee0555cbdc1b6c5867016145
mromsel <miriamromero2@gmail.com>
2024-04-25 20:12:53 +0200

    Update language selector with new modal

--- src/app/shared/components/language-selector/language-selector.component.ts
+++ src/app/shared/components/language-selector/language-selector.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Language } from '../../interfaces/language.interface';
+import { UserInfoService } from '../../services/user-info.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-language-selector',
@@ -11,8 +13,23 @@ export class LanguageSelectorComponent implements OnInit {
   @Input() languageOrigin: Language | undefined;
   @Input() languageDestiny: Language | undefined;
 
-  constructor() { }
+  filePrefix = "../../../../assets/flags/"
 
-  ngOnInit() { }
+  originLanguageFlag: string = "../../../../assets/flags/flag_es.png";
+  destinyLanguageFlag: string = "../../../../assets/flags/flag_en.png";
+
+  selectedLanguages: string;
+
+  // languagesToSelect: Array<UsersLanguages> = new Array();
+
+  constructor(private userInfoService: UserInfoService,
+    private authService: AuthService
+  ) {
+    this.selectedLanguages = this.languageOrigin?.isoCode + "." + this.languageDestiny?.isoCode
+  }
+
+  ngOnInit() {
+
+  }
 
 }
