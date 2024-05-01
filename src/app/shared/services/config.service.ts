61a5c76a86df365b29a7b85babbb87272ed2fa68
mromsel <miriamromero2@gmail.com>
2024-05-01 20:11:00 +0200

    Modify config service to store iso code

--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -2,38 +2,57 @@ import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { Language } from '../interfaces/language.interface';
 import { MastersService } from './masters.service';
 import { Observable, Subject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
+  prefLangKey = "pref-lang"
+
   defaultLocale: string = "en"
 
   preferredLanguage: Subject<Language> = new Subject<Language>();
 
+  preferredIsoCode: string = this.defaultLocale
+
   constructor(
-    @Inject(LOCALE_ID) public locale: string = "en", private mastersService: MastersService) {
-    this.setPreferredLanguage(locale)
+    @Inject(LOCALE_ID) public locale: string, private translateService: TranslateService, private mastersService: MastersService) {
+    let storageLang = localStorage.getItem(this.prefLangKey)
+    if (storageLang) {
+      this.preferredIsoCode = storageLang
+      this.setPreferredLanguage(this.preferredIsoCode)
+    } else {
+      this.setDefaultLanguage()
+    }
   }
 
   setPreferredLanguage(isoCode: string) {
-    console.log("set")
     let preferredLanguage = this.mastersService.getLanguages().filter(language => language.isoCode == isoCode)[0]
-    if (preferredLanguage && isoCode != undefined) {
+    console.log(preferredLanguage)
+    if (preferredLanguage && isoCode != undefined && this.preferredIsoCode != null) {
+      this.preferredIsoCode = preferredLanguage.isoCode
+      localStorage.setItem(this.prefLangKey, this.preferredIsoCode)
+      this.translateService.use(this.preferredIsoCode);
       this.preferredLanguage.next(preferredLanguage)
     } else {
       this.setDefaultLanguage()
     }
+  }
 
+  private setDefaultLanguage() {
+    localStorage.setItem(this.prefLangKey, this.defaultLocale)
+    this.translateService.use(this.defaultLocale);
+    this.preferredLanguage.next(this.mastersService.getLanguages().filter(language => language.isoCode == this.defaultLocale)[0])
   }
 
   getPreferredLanguageObservable(): Observable<Language> {
     return this.preferredLanguage.asObservable();
   }
 
-  private setDefaultLanguage() {
-    this.preferredLanguage.next(this.mastersService.getLanguages().filter(language => language.isoCode == this.defaultLocale)[0])
+  getPreferredIsoCode(): string {
+    return this.preferredIsoCode ?? this.defaultLocale
   }
 
 }
