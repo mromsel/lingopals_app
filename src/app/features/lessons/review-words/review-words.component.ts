af3bbad24c29295075aad02ff06bc25f21802af1
mromsel <miriamromero2@gmail.com>
2024-05-07 00:41:37 +0200

    Implement quiz in review words component

--- src/app/features/lessons/review-words/review-words.component.ts
+++ src/app/features/lessons/review-words/review-words.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { activityTypes } from 'src/app/shared/interfaces/user-activity.interface';
+import { WordsInQuiz } from '../interfaces/words-in-quiz.interface';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-review-words',
@@ -7,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewWordsComponent implements OnInit {
 
-  constructor() { }
+  backRoute: string = '/lessons'
+
+  reviewType: string = activityTypes[0]
+
+  listWords: WordsInQuiz[] = []
+
+  reviewTitle: string = "Review words" // TODO: TRANSLATE
+
+  constructor(
+    private router: Router,
+    private navController: NavController,
+  ) { }
 
   ngOnInit() { }
+
+  exitView() {
+    this.navController.pop()
+    // this.navController.navigateBack()
+    this.router.navigate(['/lessons'])
+  }
 
 }
