6c0655e6f0be1ad3e10bbe46b903b1e3df910889
mromsel <miriamromero2@gmail.com>
2024-05-07 19:26:50 +0200

    Add results property to user activity interface

--- src/app/shared/interfaces/user-activity.interface.ts
+++ src/app/shared/interfaces/user-activity.interface.ts
@@ -1,11 +1,13 @@
+import { ActivityResult } from "src/app/shared/interfaces/activity-result.interface"
 
 export interface UserActivity {
-    idUserActivity: number,
+    idUserActivity?: number,
     idUser: number,
     type: string,
     xpGained?: number,
     idLesson?: number,
-    date?: Date
+    date?: Date,
+    results: ActivityResult[]
 }
 
 export const activityTypes: string[] = ["REVIEW", "LESSON"]
