b75c617dc3d0e4f0ea9c4f69715b61192d6cf1de
mromsel <miriamromero2@gmail.com>
2024-05-07 19:24:39 +0200

    Add quiz finished card UI and input properties

--- src/app/features/lessons/components/quiz/quiz-finished-card/quiz-finished-card.component.ts
+++ src/app/features/lessons/components/quiz/quiz-finished-card/quiz-finished-card.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserActivity } from 'src/app/shared/interfaces/user-activity.interface';
+import { WordsInQuiz } from '../../../interfaces/words-in-quiz.interface';
 
 @Component({
   selector: 'app-quiz-finished-card',
   templateUrl: './quiz-finished-card.component.html',
   styleUrls: ['./quiz-finished-card.component.scss'],
 })
-export class QuizFinishedCardComponent  implements OnInit {
+export class QuizFinishedCardComponent implements OnInit {
+
+  @Input() listWords: WordsInQuiz[] | undefined;
+  @Input() userActivityResult: UserActivity | undefined;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
