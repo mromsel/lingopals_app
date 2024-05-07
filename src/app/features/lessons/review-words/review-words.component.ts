2f906a29e7621734b1b3abf19a32a41df8b1e4ea
mromsel <miriamromero2@gmail.com>
2024-05-07 19:30:30 +0200

    Program logic in review words component to fetch correspondent words

--- src/app/features/lessons/review-words/review-words.component.ts
+++ src/app/features/lessons/review-words/review-words.component.ts
@@ -3,6 +3,11 @@ import { activityTypes } from 'src/app/shared/interfaces/user-activity.interface
 import { WordsInQuiz } from '../interfaces/words-in-quiz.interface';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { UserInfoService } from 'src/app/shared/services/user-info.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { ReviewService } from '../services/review.service';
+import { EventsService } from 'src/app/shared/services/events.service';
+import { ReviewWords } from '../interfaces/review-words.interface';
 
 @Component({
   selector: 'app-review-words',
@@ -17,14 +22,41 @@ export class ReviewWordsComponent implements OnInit {
 
   listWords: WordsInQuiz[] = []
 
+  reviewWords: ReviewWords | undefined
+
   reviewTitle: string = "Review words" // TODO: TRANSLATE
 
   constructor(
+    private reviewService: ReviewService,
+    private userInfoService: UserInfoService,
+    private authService: AuthService,
+    private eventsService: EventsService,
     private router: Router,
     private navController: NavController,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    if (this.authService.getIdUser() && this.userInfoService.getUserLanguages()[0]) {
+      this.fetchWords()
+    } else {
+      this.exitView()
+    }
+  }
+
+  fetchWords() {
+    if (this.authService.getIdUser()) {
+      this.reviewService.getReviewWords(this.authService.getIdUser(), this.userInfoService.getUserLanguages()[0]).subscribe(
+        userReviewWords => {
+          this.eventsService.showSpinner$.next(true);
+          this.reviewWords = userReviewWords
+
+          this.listWords = this.reviewWords.wordsList
+
+          this.eventsService.showSpinner$.next(false);
+        }
+      )
+    }
+  }
 
   exitView() {
     this.navController.pop()
