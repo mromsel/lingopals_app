b8398a12fbd4c3a33b9f42edb7ff1bb593b1b244
mromsel <miriamromero2@gmail.com>
2024-05-11 12:49:45 +0200

    Add userLanguages to review words interface

--- src/app/features/lessons/interfaces/review-words.interface.ts
+++ src/app/features/lessons/interfaces/review-words.interface.ts
@@ -1,6 +1,8 @@
+import { UserLanguages } from "src/app/shared/interfaces/user-languages.interface";
 import { WordsInQuiz } from "./words-in-quiz.interface";
 
 export interface ReviewWords {
     languageLevel?: any,
+    userLanguages: UserLanguages,
     wordsList: WordsInQuiz[]
 }
