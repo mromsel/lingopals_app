e404adeddf0a69f8a41f54229b76df9aed0d3393
mromsel <miriamromero2@gmail.com>
2024-05-11 12:50:29 +0200

    Add userLanguages property to UserActivity

--- src/app/shared/interfaces/user-activity.interface.ts
+++ src/app/shared/interfaces/user-activity.interface.ts
@@ -1,8 +1,10 @@
 import { ActivityResult } from "src/app/shared/interfaces/activity-result.interface"
+import { UserLanguages } from "./user-languages.interface"
 
 export interface UserActivity {
     idUserActivity?: number,
     idUser: number,
+    userLanguages?: UserLanguages,
     type: string,
     xpGained?: number,
     idLesson?: number,
