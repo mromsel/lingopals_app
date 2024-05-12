53755e9ac520d41e20a23a206442c35dc84eebfc
mromsel <miriamromero2@gmail.com>
2024-05-12 18:37:31 +0200

    Add arrays with constants to display admin panel options

--- src/app/features/settings/admin-panel/admin-panel.component.ts
+++ src/app/features/settings/admin-panel/admin-panel.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
+  backRoute: string = "../settings"
+
+  dropdownParents: string[] = [
+    "Masters",
+    "Words related",
+    "Users related"
+  ]
+
+  masters: string[] = [
+    "activity-types",
+    "language-levels",
+    "languages",
+    "profiles",
+    "writing-systems",
+    "xp-levels"
+  ]
+
+  wordsRelated: string[] = [
+    "word-references",
+    "words"
+  ]
+
+  usersRelated: string[] = [
+    "users",
+    "users-progress"
+  ]
+
   constructor() { }
 
   ngOnInit() { }
