3f5bc43e55ae8f59c81ddb3c40849c0b23d44e12
mromsel <miriamromero2@gmail.com>
2024-05-12 21:06:22 +0200

    Add admin panel service basic fetching method

--- src/app/features/settings/admin-panel/services/admin-panel.service.ts
+++ src/app/features/settings/admin-panel/services/admin-panel.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminPanelService {
 
-  constructor() { }
+  backendURL: string = environment.backendURL
+
+  constructor(private http: HttpClient) { }
+
+  getResource(resource: string) {
+    return this.http.get<any>(`${this.backendURL}/${resource}`)
+  }
 }
