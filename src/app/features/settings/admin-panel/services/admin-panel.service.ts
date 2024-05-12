52257e341d243829a85eeb868b999253b84ee4b0
mromsel <miriamromero2@gmail.com>
2024-05-12 23:16:23 +0200

    Include getWordDetailsFromDictionaryAPI method in AdminPanelService

--- src/app/features/settings/admin-panel/services/admin-panel.service.ts
+++ src/app/features/settings/admin-panel/services/admin-panel.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { DictionaryApiWordData } from '../dictionary-api/dictionary-api-word-data.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminPanelService {
+
+  dictionaryApiURL = "https://api.dictionaryapi.dev/api/v2/entries/en"
 
   backendURL: string = environment.backendURL
 
@@ -13,5 +16,9 @@ export class AdminPanelService {
 
   getResource(resource: string) {
     return this.http.get<any>(`${this.backendURL}/${resource}`)
+  }
+
+  getWordDetailsFromDictionaryAPI(word: string) {
+    return this.http.get<DictionaryApiWordData[]>(`${this.dictionaryApiURL}/${word}`)
   }
 }
