d1f91f2fb64c75172efb872541fa008ae31065ed
mromsel <miriamromero2@gmail.com>
2024-05-13 18:55:08 +0200

    Add ui and logic to masters component with dynamic master fetching

--- src/app/features/settings/admin-panel/masters/masters.component.ts
+++ src/app/features/settings/admin-panel/masters/masters.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminPanelService } from '../services/admin-panel.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { adminPanelMasters } from '../interfaces/admin-panel-constants';
 
 @Component({
   selector: 'app-masters',
@@ -7,8 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MastersComponent implements OnInit {
 
-  constructor() { }
+  masters: string[] = adminPanelMasters
 
-  ngOnInit() { }
+  masterData: any[] = []
+  displayList: any[] = [];
+  displayColumns: any[] = []
+
+  currentMaster: string = ""
+  selectedProperty: string = ""
+
+
+  public showForm: boolean = false
+  public showSearchBar: boolean = true
+
+  constructor(
+    private adminPanelService: AdminPanelService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const type = params.get('type');
+      if (type) {
+        this.currentMaster = type
+        this.adminPanelService.getResource(type).subscribe(
+          data => {
+            this.masterData = data
+            this.displayList = data
+            console.log(this.masterData)
+            this.displayColumns = this.getColumnNames()
+            console.log(this.displayColumns)
+
+            if (this.displayColumns && this.displayColumns.length > 0) {
+              this.selectedProperty = this.displayColumns[0];
+            }
+          }
+        )
+      }
+    });
+  }
+
+  toggleShowForm() {
+    this.showForm = !this.showForm
+    if (this.showForm) {
+      this.showSearchBar = false
+    }
+  }
+
+  toggleShowSearchBar() {
+    this.showSearchBar = !this.showSearchBar
+    if (this.showSearchBar) {
+      this.showForm = false
+    }
+  }
+
+  action() { }
+
+  onSearch(event: any) {
+    const searchTerm = event.target.value;
+    this.displayList = this.masterData.filter(item => item[this.selectedProperty].toLowerCase().includes(searchTerm.toLowerCase()))
+
+    console.log(this.displayList)
+    console.log(this.selectedProperty)
+  }
+
+  onSelectChange(event: any) {
+    this.selectedProperty = event.detail.value;
+    console.log(this.selectedProperty)
+  }
+
+  /** Method to obtain column names depending on objects in property masterData*/
+  getColumnNames(): string[] {
+    if (this.masterData.length === 0) {
+      return [];
+    }
+    // Get properties from first element in array
+    return Object.keys(this.masterData[0]);
+  }
+
+  changeMaster(master: string) {
+    this.router.navigate([`app/settings/admin-panel/masters/${master}`])
+  }
 
 }
