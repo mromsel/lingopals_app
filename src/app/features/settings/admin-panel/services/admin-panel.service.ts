5a0c3dd53637f6a7167cbb811e37e05c5d3a6f56
mromsel <miriamromero2@gmail.com>
2024-05-15 17:45:09 +0200

    Add fetchAllMasters to admin panel service

--- src/app/features/settings/admin-panel/services/admin-panel.service.ts
+++ src/app/features/settings/admin-panel/services/admin-panel.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { DictionaryApiWordData } from '../dictionary-api/dictionary-api-word-data.interface';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,20 @@ export class AdminPanelService {
 
   backendURL: string = environment.backendURL
 
+  masters = new Subject<any[]>();
+
+  mastersStored: any[] = []
+
   constructor(private http: HttpClient) { }
+
+  fetchAllMasters() {
+    this.http.get<any>(`${this.backendURL}/masters`).subscribe(
+      masters => {
+        this.masters.next(masters)
+        this.mastersStored = masters
+      }
+    )
+  }
 
   getResource(resource: string) {
     return this.http.get<any>(`${this.backendURL}/${resource}`)
