1c275d62fea8cd0d3d20d1d2befcabf291cd5869
mromsel <miriamromero2@gmail.com>
2024-05-19 18:16:20 +0200

    Change tabs bar styles and icons

--- src/app/features/tabs/tabs.page.ts
+++ src/app/features/tabs/tabs.page.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
 })
-export class TabsPage implements OnInit {
+export class TabsPage {
+
+  @ViewChild('tabs', { static: false }) tabs: IonTabs | undefined;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.tabs?.ionTabsDidChange.subscribe(() => {
+      this.updateTabSelection();
+    });
+    this.updateTabSelection();
+  }
+  updateTabSelection() {
+    const selectedTab = this.tabs?.getSelected();
+    const tabButtons = document.querySelectorAll('ion-tab-button');
+    tabButtons.forEach((tabButton) => {
+      if (tabButton.getAttribute('tab') === selectedTab) {
+        tabButton.classList.add('tab-button-selected');
+      } else {
+        tabButton.classList.remove('tab-button-selected');
+      }
+    });
   }
 }
