ed4d72aae83368a42b0c6ff8ac80347a9ff32d9b
mromsel <miriamromero2@gmail.com>
2024-05-23 20:25:53 +0200

    Fix search in dictionary and UI

--- src/app/features/dictionary/dictionary.page.ts
+++ src/app/features/dictionary/dictionary.page.ts
@@ -4,6 +4,7 @@ import { Subject, takeUntil } from 'rxjs';
 import { UserLanguages } from 'src/app/shared/interfaces/user-languages.interface';
 import { Word } from 'src/app/shared/interfaces/word.interface';
 import { ConfigService } from 'src/app/shared/services/config.service';
+import { UserInfoService } from 'src/app/shared/services/user-info.service';
 import { WordsService } from 'src/app/shared/services/words.service';
 
 @Component({
@@ -25,46 +26,59 @@ export class DictionaryPage {
 
   constructor(
     private wordsService: WordsService,
+    private userInfoService: UserInfoService,
     private configService: ConfigService,
     private router: Router
   ) { }
 
   ionViewWillEnter() {
-    this.configService.preferredUserLanguages
+    let userLanguages = this.userInfoService.userLanguages
+    if (userLanguages) {
+      this.getAllWordsByLanguage(userLanguages.filter(userLanguage => userLanguage.preferred)[0])
+    }
+
+    this.configService.preferredUserLanguagesSubject
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(
         preferredUserLanguages => {
           this.preferredUserLanguages = preferredUserLanguages
+          this.getAllWordsByLanguage(this.preferredUserLanguages)
+        }
+      )
+  }
 
-          this.wordsService.getAllWordsByLanguage(this.preferredUserLanguages.languageTarget.isoCode)
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(
-              data => {
-                this.words = data
-                // #TEST
-                this.words.forEach(element => {
-                  this.words.push(element)
-                });
-                this.words.forEach(element => {
-                  this.words.push(element)
-                });
-                // #END TEST
+  getAllWordsByLanguage(preferredUserLanguages: UserLanguages) {
+    this.wordsService.getAllWordsByLanguage(preferredUserLanguages.languageTarget.isoCode)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        data => {
+          this.words = data
+          // #TEST
+          this.words.forEach(element => {
+            this.words.push(element)
+          });
+          this.words.forEach(element => {
+            this.words.push(element)
+          });
+          // #END TEST
 
-                this.sortWords();
-              }
-            )
+          this.sortWords();
         }
       )
   }
 
   sortWords() {
-    this.words.sort((a, b) => ('' + a.wordString).localeCompare(b.wordString)); // Ordenar alfabéticamente
+    this.words.sort((a, b) => ('' + a.wordString).localeCompare(b.wordString)); // Order list
 
-    // Separar palabras por letra
+    // Split by letter
+    /* Create an array for each letter and fill it with
+     words starting with that letter*/
     this.alphabet.forEach(letter => {
       this.sortedWords[letter] = this.words.filter(word => word.wordString.startsWith(letter));
     });
-    // Separar palabras por letra
+    /* Iterate over each word and check if the first letter
+     is in the alphabet, then add it to the corresponding
+     array */
     this.words.forEach(word => {
       const firstLetter = word.wordString.charAt(0).toUpperCase();
       if (this.alphabet.includes(firstLetter)) {
@@ -73,13 +87,44 @@ export class DictionaryPage {
     });
   }
 
-
   goToDetail(word: Word) {
-    this.router.navigate(['app/dictionary/word-details/' + word.idWord])
+    this.router.navigate(['app/dictionary/word-details/' + word.idWord], { state: word })
   }
 
   ionViewWillLeave() {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  onSearch(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+
+    if (searchTerm === "") {
+      this.sortWords(); // Reset to full list of words
+    } else {
+      this.sortedWords = {};
+
+      // Filter words by searchTerm
+      const filteredWords = this.words.filter(word =>
+        word.wordString.toLowerCase().includes(searchTerm)
+      );
+
+      // Organize words by letter
+      filteredWords.forEach(word => {
+        const firstLetter = word.wordString.charAt(0).toUpperCase();
+        if (!this.sortedWords[firstLetter]) {
+          this.sortedWords[firstLetter] = [];
+        }
+        this.sortedWords[firstLetter].push(word);
+      });
+
+      // Navigates to first letter
+      const componente = document.getElementById(searchTerm.charAt(0).toUpperCase());
+
+      if (componente != null) {
+        componente.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        // this.toggleActive(sectionId)
+      }
+    }
   }
 }
