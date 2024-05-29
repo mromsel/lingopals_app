ca5631f88245ad389a269fa2779f9a77fe5a7609
mromsel <miriamromero2@gmail.com>
2024-05-29 21:47:56 +0200

    Fix signup page logic in form

--- src/app/features/signup/signup.page.ts
+++ src/app/features/signup/signup.page.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserSignUp } from 'src/app/shared/interfaces/user-signup.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { ConfigService } from 'src/app/shared/services/config.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class SignupPage {
 
   formData = {
     username: '',
@@ -16,15 +19,35 @@ export class SignupPage implements OnInit {
   };
 
   constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
     private router: Router
   ) { }
-
-  ngOnInit() {
-  }
 
   goToLogin() {
     this.router.navigate(["/login"])
   }
 
-  signup() { }
+  signup() {
+    // TODO: COMPROBACIONES
+    console.log("signup method");
+
+    if (this.formData.password == this.formData.confirmPassword) {
+
+      let preferredLanguage = this.configService.preferredLanguage?.idLanguage ?? 1
+
+      let userSignUp: UserSignUp = {
+        username: this.formData.username,
+        email: this.formData.email,
+        password: this.formData.password,
+        idPreferredLanguage: preferredLanguage,
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+      }
+      console.log(userSignUp);
+
+      this.authService.signup(userSignUp).subscribe(
+        user => console.log(user)
+      )
+    }
+  }
 }
