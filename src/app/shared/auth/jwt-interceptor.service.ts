63721deff88f9ba0def525ab84c847f33a609e96
mromsel <miriamromero2@gmail.com>
2024-06-04 18:14:29 +0200

    Create JwtInterceptorService to send token in request

---
+++ src/app/shared/auth/jwt-interceptor.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtInterceptorService implements HttpInterceptor {
+
+  constructor(private localStorageService: LocalStorageService,
+  ) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token: string | null = this.localStorageService.getItem(this.localStorageService.TOKEN_KEY);
+
+    if (token != "" && token != null) {
+
+      req = req.clone(
+        {
+          setHeaders: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        }
+      );
+    }
+    return next.handle(req);
+  }
+}
