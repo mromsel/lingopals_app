0a94ec0c7ad2b8f6212b44aa11595c6e39892006
mromsel <miriamromero2@gmail.com>
2024-06-06 18:11:13 +0200

    Implement jwt and error interceptors in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginPageRoutingModule } from './features/login/login-routing.module';
 import { SignupPageRoutingModule } from './features/signup/signup-routing.module';
 import { SharedModule } from './shared/module/shared.module';
 import { TabsPageModule } from './features/tabs/tabs.module';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { createTranslateLoader } from 'src/main';
+import { JwtInterceptorService } from './shared/auth/jwt-interceptor.service';
+import { ErrorInterceptorService } from './shared/errors/error-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -40,7 +42,11 @@ import { createTranslateLoader } from 'src/main';
     SignupPageRoutingModule,
     TabsPageModule,
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
