89ad05add4417e1a396826143560e4e0f343dc01
mromsel <miriamromero2@gmail.com>
2024-06-07 17:10:47 +0200

    Fix home header display info

--- src/app/features/home/components/home-header/home-header.component.ts
+++ src/app/features/home/components/home-header/home-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { UserProgress } from 'src/app/shared/interfaces/user-related/user-progress.interface';
 import { UserInfoService } from 'src/app/shared/services/user-related/user-info.service';
 
@@ -7,9 +7,9 @@ import { UserInfoService } from 'src/app/shared/services/user-related/user-info.
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss'],
 })
-export class HomeHeaderComponent implements OnInit {
+export class HomeHeaderComponent {
 
-  userProgress: UserProgress | undefined
+  @Input() userProgress: UserProgress | undefined
 
   levelProgressValue: number = 0
 
@@ -17,18 +17,4 @@ export class HomeHeaderComponent implements OnInit {
     private userInfoService: UserInfoService,
   ) { }
 
-  ngOnInit() {
-    if (this.userInfoService.idUser) {
-      this.userInfoService.getUserProgress(this.userInfoService.idUser).subscribe(
-        userProgress => {
-          this.userProgress = userProgress
-
-          let xpLevel = this.userProgress.xpLevel
-
-          let xpProgress = (this.userProgress.xpPoints - xpLevel.xpRangeStart) / (xpLevel.xpRangeEnd - xpLevel.xpRangeStart)
-          this.levelProgressValue = xpProgress
-        }
-      )
-    }
-  }
 }
